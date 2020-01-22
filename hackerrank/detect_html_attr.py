4ddb775a19fb2d5140de18c3f7d4d3e480a3c690
SaiKumar <ksaikumar2505@gmail.com>
2020-01-22 23:04:07 +0530

    Update detect_html_attr.py

--- hackerrank/detect_html_attr.py
+++ hackerrank/detect_html_attr.py
@@ -1,30 +1,25 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-import re
+#https://www.hackerrank.com/challenges/html-attributes/problem
+re
 import sys
 #sys.stdin.read()
 N = raw_input()
 result =[]
+d={}
 for i in range(int(N)):
     #res = re.findall(r'(<\w+>|<\w+\s+\w+|\w+=)',inp)
     res = re.findall(r'<(\w+)(|\s+[^>]*)>',raw_input())
-    #<(\w+)(|\s+[^>]*)>  (\w+)=[\'\"]
-    #print(res)
-    #res1 = map(lambda x:re.sub(r'[^\s\w]','',x),res)
-    #res1 = map(lambda x:x.replace(" ",":").replace("<","").replace(">",":"),res)
-    #print(res1)
-    #r1 = list(set(res1))
-    #print(r1)
-    #res2 = map(lambda x:re.sub(r'\s',':',x),res1)
+    res1 = map(lambda x:[x[0],sorted(re.findall(r'(\w+)=[\'\"]',x[1]))],res)
+    res2 = list(res1)
     #print(res2)
-    res1 = map(lambda x:[x[0],",".join(sorted(re.findall(r'(\w+)=[\'\"]',x[1])))],res)
-    #print(res1)
-    res2 = map(lambda x:':'.join(x),res1)
-    #print(res2)
-    
-    result.extend(res2)
-print(result)
-r1 = list(set(result))
-print(r1)
-rs = sorted(r1)
-print("\n".join(rs))
+    ikeys = list(map(lambda x:x[0],res))
+    for i in list(res2):
+        if i[0] not in d.keys():
+            d[i[0]]=i[1]
+        else:
+            d[i[0]]+=i[1]
+
+#print(d)
+
+for k, v in d.items():
+    result.append(k+':'+",".join(sorted(list(set(v)))))
+print("\n".join(sorted(result)))
